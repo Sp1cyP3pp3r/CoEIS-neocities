0f1f5eac5f23724ffbbcc7452b186f0c60551f63
Sp1cyP3pp3r <niktoktoto21@gmail.com>
2026-07-12 17:40:04 +0400

    Update comments.js

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -254,7 +254,7 @@ function renderCommentTree(comment, allComments, depth = 0) {
 
   // Avatar (CSS variable, no <img>)
   const avatarStyle = comment.avatar_url
-    ? `style="--avatar-url: url('${comment.avatar_url.replace(/'/g, "\\'")}')"`
+    ? `style="--avatar-url: url(${comment.avatar_url})"`
     : "";
 
   // Neocities link
