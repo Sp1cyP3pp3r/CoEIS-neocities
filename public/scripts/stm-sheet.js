2b83f71ed0c4d3c3503220a8c1bb6d217ffdbec6
Sp1cyP3pp3r <niktoktoto21@gmail.com>
2026-08-02 15:15:06 +0400

    Update stm-sheet.js

--- public/scripts/stm-sheet.js
+++ public/scripts/stm-sheet.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Находим все чекбоксы на странице
-  const checkboxes = document.querySelectorAll(".row-box");
+  // Превращаем коллекцию чекбоксов в массив и разворачиваем его
+  const checkboxes = Array.from(
+    document.querySelectorAll(".row-box"),
+  ).reverse();
 
   checkboxes.forEach((checkbox, index) => {
     checkbox.addEventListener("change", function () {
-      // Если чекбокс отмечен, отмечаем все последующие
       if (this.checked) {
+        // Теперь "последующие" элементы в массиве — это элементы левее на экране
         for (let i = index + 1; i < checkboxes.length; i++) {
           checkboxes[i].checked = true;
         }
