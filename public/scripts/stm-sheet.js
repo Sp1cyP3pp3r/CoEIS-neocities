1c4ae3350828ca3556002f69cf5a9cb213e8034c
Sp1cyP3pp3r <niktoktoto21@gmail.com>
2026-08-02 15:14:20 +0400

    script

--- public/scripts/stm-sheet.js
+++ public/scripts/stm-sheet.js
@@ -1,14 +1,15 @@
-// Select all checkboxes in the row
-const checkboxes = document.querySelectorAll(".row-box");
+document.addEventListener("DOMContentLoaded", () => {
+  // Находим все чекбоксы на странице
+  const checkboxes = document.querySelectorAll(".row-box");
 
-checkboxes.forEach((checkbox, index) => {
-  checkbox.addEventListener("change", function () {
-    // Only trigger logic if the checkbox is being checked
-    if (this.checked) {
-      // Loop through all checkboxes after the current one
-      for (let i = index + 1; i < checkboxes.length; i++) {
-        checkboxes[i].checked = true;
+  checkboxes.forEach((checkbox, index) => {
+    checkbox.addEventListener("change", function () {
+      // Если чекбокс отмечен, отмечаем все последующие
+      if (this.checked) {
+        for (let i = index + 1; i < checkboxes.length; i++) {
+          checkboxes[i].checked = true;
+        }
       }
-    }
+    });
   });
 });
